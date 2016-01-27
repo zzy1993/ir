1e690823a836c51b9097427533c4a29d1a839d94
zzy1993 <zzy1993@163.com>
2016-01-26 20:34:27 -0500

    Using linkSet & linkSetComp, Writing to file

--- as1.py
+++ as1.py
@@ -6,43 +6,49 @@
 import urllib2
 import time
 import re
+import json
 
-def get_text(link, timeDelay):
-    text = urllib2.urlopen(link).read()
-    time.sleep(timeDelay)
-    return text
-
-def get_links(text):
+def get_adj_links(link, timeDelay):
+	text = urllib2.urlopen(link).read()
+	time.sleep(timeDelay)
 	pattern = 'a href="(\/wiki\/[\w_\-.#]+)"'
 	linkAffixes = re.findall(pattern, text)
 	links = ['https://en.wikipedia.org'+linkAffix for linkAffix in linkAffixes]
 	return links
 
-def del_repeats(linksGet, linkSet):
-	links = list(set(linksGet))
-	linkSet = list(set(linkSet+links))
-	return linkSet
-
-def crawl(linkSet, level, levelLimit, timeDelay):
-	links = linkSet
-	for level in range(level, levelLimit+1):
-		for link in links:
-			text = get_text(link, timeDelay)
-			linksGet = get_links(text)
-			linkSet2 = del_repeats(linksGet, linkSet)
-			count = len(linkSet2)
-			print 'Count: ' + str(count)
+def search(link, depth, depthLimit, timeDelay):
+	linkSet = set()
+	linkSet.add(link)
+	count = len(linkSet)
+	linkSetComp = linkSet
+	setList = list()
+	setList.append(linkSetComp)
+	for depth in range(depth, depthLimit):
+		depth = depth + 1
+		linkSetComp2 = set()
+		for link in linkSetComp:
 			if count > 1000:
 				break
-			links = [link for link in linkSet2 if link not in linkSet]
+			linksAdj = get_adj_links(link, timeDelay)
+			linkSet2 = linkSet | set(linksAdj)
+			linkSetComp2 |= (linkSet2 - linkSet)
 			linkSet = linkSet2
-		print str(linkSet) + '\nTotal ' + str(count) + ' links crawled by level ' + str(level)
-	return linkSet
+			count = len(linkSet)
+		linkSetComp = linkSetComp2
+		setList.append(linkSetComp)
+		print json.dumps(list(linkSetComp), sort_keys=True, indent=4, separators=(',', ': ')) + '\nTotal ' + str(count) + ' links crawled \'til here depth ' + str(depth)
+	return setList, linkSet
+
+def fwrite(list, path):
+	with open(path, 'wb') as f:
+		f.write(json.dumps(list, sort_keys=True, indent=4, separators=(',',':')))
+
 
 if __name__ == '__main__':
-	linkSet = ['https://en.wikipedia.org/wiki/Sustainable_Energy']
-	level = 0
-	levelLimit = 2
+	linkStart = 'https://en.wikipedia.org/wiki/Sustainable_Energy'
+	depth = 0
+	depthLimit = 2
 	timeDelay = 1
-	links = crawl(linkSet, level, levelLimit, timeDelay)
-	print links
+	path = '/Users/Aleph/Desktop/IR/as1-1.txt'
+	setList, linkSet = search(linkStart, depth, depthLimit, timeDelay)
+	fwrite(list(linkSet), path)
